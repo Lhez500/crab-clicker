4a474e670d3ee6d87510672862945c9c833873e3
Lhez500 <152247586+Lhez500@users.noreply.github.com>
2025-01-02 16:37:41 -0800

    Update connection.php

--- 2ndCrab/connection.php
+++ 2ndCrab/connection.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $connection = new PDO ('mysql:host=localhost;port=3306;dbname=accounts', 'root', '');
+    $connection = new PDO ('mysql:host="enter your host here";port="port goes here";dbname=""db name', '"name"', '"password"');
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
     echo 'connection error';
